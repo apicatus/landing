a498be99647d22faea4251c5f51122d7f6225f76
Benjamin Maggi <benjaminmaggi@gmail.com>
2014-12-22 19:34:58 -0300

    new all

--- public/js/aplication.js
+++ public/js/aplication.js
@@ -7,10 +7,14 @@ var Landing = angular.module('Landing', [
     'ui.router',
     'ui.utils',
     'ngAnimate',
-    'restangular'
+    'restangular',
+    'duScroll'
 ]);
+Landing.constant('skrollr', skrollr);
+Landing.constant('Modernizr', Modernizr);
+Landing.constant('Trianglify', Trianglify);
 Landing.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 'RestangularProvider', function($stateProvider, $urlRouterProvider, $locationProvider, RestangularProvider) {
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('!');
     RestangularProvider.setRestangularFields({
         id: '_id'
@@ -27,6 +31,31 @@ Landing.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 'Re
                     templateUrl: 'home.html',
                     controller: 'HomeCtrl as home'
                 },
+                'nav': {
+                    templateUrl: 'nav.html',
+                },
+                'footer': {
+                    templateUrl: 'footer.html',
+                },
+                'login': {
+                    templateUrl: 'login.html',
+                    controller: 'LoginCtrl as login'
+                }
+            }
+        })
+        .state('features', {
+            url: '/features',
+            views: {
+                'main': {
+                    templateUrl: 'tos.html',
+                    controller: 'HomeCtrl as home'
+                },
+                'nav': {
+                    templateUrl: 'nav-simple.html',
+                },
+                'footer': {
+                    templateUrl: 'footer.html',
+                },
                 'login': {
                     templateUrl: 'login.html',
                     controller: 'LoginCtrl as login'
@@ -39,33 +68,107 @@ Landing.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 'Re
                 'main': {
                     templateUrl: 'tos.html',
                     controller: 'HomeCtrl as home'
+                },
+                'nav': {
+                    templateUrl: 'nav-simple.html',
+                },
+                'footer': {
+                    templateUrl: 'footer.html',
+                },
+                'login': {
+                    templateUrl: 'login.html',
+                    controller: 'LoginCtrl as login'
                 }
             }
         });
 
 }]);
-Landing.run(['$rootScope', '$state', 'Restangular', function($rootScope, $state, Restangular) {
-
-    $rootScope.user = {
-        name: 'Eltiro',
+// http://stackoverflow.com/questions/23715337/integrating-skrollr-w-angularjs-single-page-app
+Landing.directive('skrollr', function() {
+    var directiveDefinitionObject = {
+        link: function(scope, element, attrs) {
+            var skrl = skrollr.init({
+                forceHeight: false
+            });
+            setTimeout(function(){
+                skrl.refresh();
+            }, 500);
+            //This will watch for any new elements being added as children to whatever element this directive is placed on. If new elements are added, Skrollr will be refreshed (pulling in the new elements
+           scope.$watch(
+               function () { return element[0].childNodes.length; },
+               function (newValue, oldValue) {
+               if (newValue !== oldValue) {
+                    skrl = skrollr.init({
+                        forceHeight: false
+                    });
+                    skrl.refresh();
+               }
+           });
+        }
+    };
+    if(Modernizr.mq('only screen and (min-width: 800px)')){
+        return directiveDefinitionObject;
+    } else {
+        return {};
     }
-    Restangular.oneUrl('user', 'http://api.apicat.us:8070/user/signin').customPOST({username: 'admin', password: 'admin'}).then(function(response){
-        console.log(response)
+});
+Landing.directive('trianglify', ['$interval', function($interval){
+    function scale(valueIn, baseMin, baseMax, limitMin, limitMax) {
+        return ((limitMax - limitMin) * (valueIn - baseMin) / (baseMax - baseMin)) + limitMin;
+    }
+    return {
+        restrict: 'A',
+        scope: {
+            cellsize: '@',
+            bleed: '@',
+            cellpadding: '@',
+            noiseIntensity: '@',
+            txGradient: '=',
+            tyGradient: '=',
+            fillOpacity: '@',
+            strokeOpacity: '@'
+        },
+        link: function(scope, element, attrs) {
+            console.log("scope", scope);
+            var polygons = new Trianglify({
+                cellsize: scope.cellsize,
+                bleed: scope.bleed,
+                cellpadding: scope.cellpadding,
+                noiseIntensity: scope.noiseIntensity || 0,
+                x_gradient: scope.txGradient,
+                y_gradient: scope.tyGradient,
+                fillOpacity: scope.fillOpacity || 1,
+                strokeOpacity: scope.strokeOpacity || 1
+            });
+            var pattern = polygons.generate(element.width(), element.height());
+            element.css('background-image', pattern.dataUrl);
+        }
+    };
+}]);
+Landing.run(['$rootScope', '$state', 'Restangular', function($rootScope, $state, Restangular) {
+    $rootScope.$on('$stateChangeSuccess',function(){
+        $("html, body").animate({ scrollTop: 0 }, 200);
     });
-
+    $rootScope.$on('duScrollspy:becameActive', function($event, $element){
+        $('nav.menu > input[type=checkbox]').removeAttr('checked');
+    });
 }]);
 Landing.controller('HomeCtrl', ['$scope', function($scope){
+    var home = this;
+    home.login = {};
+    home.login.state = 'continue';
 
+    home.login.recover = function() {
+        home.login.state = 'recover';
+    };
 }]);
 Landing.controller('LoginCtrl', ['$scope', '$window', function($scope, $window){
-    console.log('LoginCtrl');
-
     this.state = 'continue';
     this.recover = function() {
         this.state = 'recover';
     };
 }]);
-Landing.controller('MainCtrl', ['$scope', '$window', function($scope, $window){
+Landing.controller('MainCtrl', ['$rootScope', '$scope', '$window', function($rootScope, $scope, $window){
     this.goAuth = function(provider) {
         $window.location.assign('/auth/' + provider);
     };
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,14 @@
 $(function () {
+
     // Fixed nav after certain point
     $(window).scroll(function () {
         var scale = (Math.abs(scroll) / 1000) < 3 ? (Math.abs(scroll) / 1000) : 1;
         var scroll = $(this).scrollTop();
 
         if (scroll > 456) {
-            $('#menu').addClass("f-nav");
+            $('nav.menu:not(.simple)').addClass("f-nav");
         } else {
-            $('#menu').removeClass("f-nav");
+            $('nav.menu:not(.simple)').removeClass("f-nav");
         }
         if (scroll < 0) {
             $('.icon32').css({
@@ -22,65 +23,8 @@ $(function () {
         }
     });
 
-    // Animated Scroll
-    $('a[href*=#]:not([href=#])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
-
-    // Cache selectors
-    var lastId,
-        topMenu = $("#menu"),
-        topMenuHeight = topMenu.outerHeight() + 15,
-        // All list items
-        menuItems = topMenu.find("a"),
-        // Anchors corresponding to menu items
-        scrollItems = menuItems.map(function () {
-            var item = $($(this).attr("href"));
-            if (item.length) {
-                return item;
-            }
-        });
-
-    // Bind to scroll
-    $(window).scroll(function () {
-        // Get container scroll position
-        var fromTop = $(this).scrollTop() + topMenuHeight;
-
-        // Get id of current scroll item
-        var cur = scrollItems.map(function () {
-            if ($(this).offset().top < fromTop) {
-                return this;
-            }
-        });
-
-        // Get the id of the current element
-        cur = cur[cur.length - 1];
-        var id = cur && cur.length ? cur[0].id : "";
-
-        if (lastId !== id) {
-            lastId = id;
-            // Set/remove active class
-            menuItems
-                .parent()
-                .removeClass("active")
-                .end()
-                .filter("[href=#" + id + "]")
-                .parent()
-                .addClass("active");
-        }
-    });
-
     $(".nav-btn").click(function () {
-        $('#menu .block > input[type=checkbox]').removeAttr('checked');
+        console.log("clickky!")
+        $('nav.menu > input[type=checkbox]').removeAttr('checked');
     });
-
 });
